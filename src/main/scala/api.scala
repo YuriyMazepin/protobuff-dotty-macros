48ebd15e073f64052fa00ee7742880629b1e93aa
Yuriy Mazepin <yuriy.mazepin@playtech.com>
2020-02-03 19:19:42 +0200

    api

--- src/main/scala/api.scala
+++ src/main/scala/api.scala
@@ -4,26 +4,26 @@ package proto
 import com.google.protobuf.{CodedOutputStream, CodedInputStream}
 
 object api {
-    trait Prepare {
-        val size: Int
-        def write(os: CodedOutputStream): Unit
-    }
+  trait Prepare {
+    val size: Int
+    def write(os: CodedOutputStream): Unit
+  }
     
-    trait MessageCodec[A] {
-        def prepare(a: A): Prepare
-        def read(is: CodedInputStream): A
-    }
+  trait MessageCodec[A] {
+    def prepare(a: A): Prepare
+    def read(is: CodedInputStream): A
+  }
+  
+  def encode[A: MessageCodec](a: A)(given c: MessageCodec[A]): Array[Byte] = {
+    val p = c.prepare(a)
+    val bytes = new Array[Byte](p.size)
+    val os = CodedOutputStream.newInstance(bytes)
+    p.write(os)
+    bytes
+  }
     
-    def encode[A](a: A)(given c: MessageCodec[A]): Array[Byte] = {
-        val p = c.prepare(a)
-        val bytes = new Array[Byte](p.size)
-        val os = CodedOutputStream.newInstance(bytes)
-        p.write(os)
-        bytes
-    }
-    
-    def decode[A](bytes: Array[Byte])(given c: MessageCodec[A]): A = {
-        val is = CodedInputStream.newInstance(bytes)
-        c.read(is)
-    }
+  def decode[A: MessageCodec](bytes: Array[Byte])(given c: MessageCodec[A]): A = {
+    val is = CodedInputStream.newInstance(bytes)
+    c.read(is)
+  }
 }
--- src/main/scala/macrosapi.scala
+++ src/main/scala/macrosapi.scala
@@ -1,6 +1,26 @@
 package mazepin
 package proto
 
+import proto.api.{MessageCodec, Prepare}
+import com.google.protobuf.{CodedOutputStream, CodedInputStream}
+import scala.quoted._
+
 object macrosapi {
-    
+  inline def casecodecAuto[A]: MessageCodec[A] = ${Impl.caseCodecAuto[A]}
+
+}
+
+object Impl {
+  def caseCodecAuto[A: Type](given QuoteContext): Expr[MessageCodec[A]] = '{
+    new MessageCodec[A] {
+      def prepare(a: A): Prepare = ???
+      def read(is: CodedInputStream): A = ${readImpl[A]}
+    }
+  }
+
+  def readImpl[A: Type]: Expr[A] = {
+    val tpe: Type[A] = summon[Type[A]]
+    ???
+  }
+
 }
