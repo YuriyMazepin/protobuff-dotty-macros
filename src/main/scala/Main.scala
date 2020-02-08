928c84230a6068142b155f0671fc2cfe180acba6
YuriyMazepin <yuriy.mazepin@gmail.com>
2020-02-08 14:07:51 +0200

    Get nums

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,16 +7,15 @@ import mazepin.proto.api.{MessageCodec, N, decode}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    // implicit val codec1: MessageCodec[Test] = casecodecAuto[Test]
-    implicit val codec1: MessageCodec[Dog.type] = casecodecAuto[Dog.type]
-    val res: Dog.type = decode(Array())
-    println(s"res ${res}")
+    // given codec1: MessageCodec[Animal] = casecodecAuto[Animal]
+    given codec2: MessageCodec[Dog.type] = casecodecAuto[Dog.type]
+    // val res: Dog.type = decode(Array())
+    // println(s"res ${res}")
   }
-
 }
 
 final case class Test() {
   def hello = "Hello"
 }
-final case class Animal(@N(1) name: String="n1", @N(2) color: String="c1", @N(3) old: Int=1)
+final case class Animal(@N(1) name: String="Tom", @N(2) color: String="Black", @N(3) old: Int=1, @N(4) tpe: String = "cat")
 case object Dog
--- src/main/scala/macrosapi.scala
+++ src/main/scala/macrosapi.scala
@@ -1,7 +1,7 @@
 package mazepin
 package proto
 
-import proto.api.{MessageCodec, Prepare}
+import proto.api.{MessageCodec, Prepare, N}
 import com.google.protobuf.{CodedOutputStream, CodedInputStream}
 import scala.quoted._
 
@@ -17,7 +17,6 @@ object Macro {
 class Impl(qctx: QuoteContext) extends Common(qctx) {
   def caseCodecAuto[A: Type]: Expr[MessageCodec[A]] = {
     val t = '[A]
-    // println(t.unseal.tpe.classSymbol.map(_.caseFields.map(_.annots)))
     '{ new MessageCodec[A] {
         def read(is: CodedInputStream): A = ${ readImpl[A](t) }
        } 
@@ -34,16 +33,31 @@ trait Common(val qctx: QuoteContext) {
   import qctx.tasty.{_, given}
   given ctx: QuoteContext = qctx
 
+  private[proto] case class FieldInfo(name: String, num: Int)
+
   def classApply[A: Tpe](t: Tpe[A]): Expr[A] = {
+
+    val nums = caseClassParams[A](t).map(p => 
+      p.annots.collect{ case Apply(Select(New(tpt),_), List(Literal(Constant(num: Int)))) if tpt.tpe.isNType => p.name -> num } match {
+        case List(x) => x
+        case Nil => qctx.error(s"missing ${NTypeSymbol.name} annotation for `${t.unseal.tpe.typeSymbol.name}`")
+        case _ => qctx.error(s"multiple ${NTypeSymbol.name} annotations applied for `${t.unseal.tpe.typeSymbol.name}`")
+      }
+    )
     t.unseal.tpe match {
-      case x @ TypeRef(_) => 
+      case x @ TypeRef(_) =>
         val sym = x.typeSymbol
         sym.companionModule.method("apply") match {
           case method :: _ => Apply(Select(Ident(TermRef(x.qualifier, sym.name)), method), Nil).seal.cast[A]
           case _ => ???
         }
-      //TermRef(ThisType(TypeRef(NoPrefix,module class proto)),module Dog)
       case y: TermRef => Ident(y).seal.cast[A]
     }
   }
+
+  val NTpe: Tpe[N] = '[N]
+  val NTypeSymbol = NTpe.unseal.tpe.typeSymbol
+  def (t: Type) isNType: Boolean = t.typeSymbol == NTypeSymbol
+  def caseClassParams[A: Tpe](t: Tpe[A]): List[Symbol] = t.unseal.tpe.typeSymbol.caseFields
+  
 }
